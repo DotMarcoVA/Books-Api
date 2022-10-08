26b4c28257a472e920228c3f059d295b02098183
Marco Valdez <antem.marco@gmail.com>
2022-10-08 17:14:39 -0500

    fix: Adiciones de configuracion de Puerto, Engine, Procfile y correccion de dependencias para hacer Deploy

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import productRoutes from "./routes/index.routes";
 const app = express();
 
 /* Setear el puerto a 4000 */
-app.set("port", 4000);
+app.set("port", process.env.PORT || 3000);
 
 /* Middlewares */
 app.use(morgan("dev"));
